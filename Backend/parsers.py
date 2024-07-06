20de5fd5ff0b4d1c6e0a80e6c81f64ec8791e563
artem-barsov <artem_barsov@mail.ru>
2024-07-06 03:01:08 +0300

    parse add command

---
+++ Backend/parsers.py
@@ -0,0 +1,13 @@
+class ParsedQuery:
+    def __init__(self, s):
+        self.debters = {}
+        self.desc = ''
+        for i, line in enumerate(s.splitlines()):
+            if i == 0:
+                self.amount = int(line.split()[1])
+            else:
+                t = " ".join(line.split())
+                if t.split()[0][0] == '@':
+                    self.debters[t.split()[0]] = (t.split()[1] if len(t.split()) > 1 else '')
+                else:
+                    self.desc += line + '\n'
