3acb2a4f72f9e4851cd532aa9e620770c81e0560
Ilep <ilep@noreply.com>
2024-07-10 22:13:04 +0300

    interfaces added

---
+++ Backend/reports.py
@@ -0,0 +1,9 @@
+# trati - {amount, creditor, debtors} // debtors['amir'] = 300
+# returns - {name, amount}[]
+def generateReport(trati):
+    pass
+
+# balances - {name, amount}[]
+# returns - {from, to, amount}[]
+def calculateTransactions(balances):
+    pass
