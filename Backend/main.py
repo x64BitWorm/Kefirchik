1049b9372a201ff60e4becae2267f39e3e4ddf0e
Ilep <ilep@noreply.com>
2025-01-26 23:58:25 +0300

    fix local docker

--- Backend/main.py
+++ Backend/main.py
@@ -15,12 +15,23 @@ logger = logging.getLogger(__name__)
 DB_RELEASE_PATH = '/var/lib/kefirchik/kefirchik.db'
 DB_DEBUG_PATH = '../Database/kefirchik.db'
 
+def setupEnv():
+    # default local run
+    use_web_hooks = False
+    db_path = DB_DEBUG_PATH
+    MODE = os.environ.get('MODE')
+    if MODE == 'release':
+        use_web_hooks = True
+        db_path = DB_RELEASE_PATH
+    elif MODE == 'grisha':
+        use_web_hooks = False
+        db_path = os.environ.get('DB_PATH')
+    return (use_web_hooks, db_path)
+
 def main() -> None:
     """Start the bot."""
     TOKEN = os.environ.get('TG_TOKEN')
-    MODE = os.environ.get('MODE')
-
-    db_path = DB_RELEASE_PATH if MODE == 'release' else DB_DEBUG_PATH
+    (use_web_hooks, db_path) = setupEnv()
     database.initDatabase(db_path)
 
     application = Application.builder().token(TOKEN).build()
@@ -32,12 +43,12 @@ def main() -> None:
     application.add_handler(CallbackQueryHandler(commands.cancel_callback, pattern='cancel-send'))
     application.add_handler(CallbackQueryHandler(commands.reset_callback, pattern='reset-costs'))
     application.add_handler(MessageHandler(filters.REPLY, commands.reply))
-    if MODE == 'release':
+    if use_web_hooks:
         application.bot.set_webhook(url=f"https://kefirchik-bot.tw1.ru:8443", allowed_updates=Update.ALL_TYPES)
         application.run_webhook(
             listen='0.0.0.0',
             port=8443,
-            secret_token='ASecretTokenIHaveChangedByNow',
+            secret_token=TOKEN,
             key='/var/lib/kefirchik/private.key',
             cert='/var/lib/kefirchik/cert.pem',
             webhook_url='https://kefirchik-bot.tw1.ru:8443'
--- README.md
+++ README.md
@@ -10,8 +10,36 @@ by 42
 Generate certificate for telegram webhook mode
 `openssl req -newkey rsa:2048 -sha256 -nodes -keyout private.key -x509 -days 3650 -out cert.pem`
 
-## Deploy
-From Deploy folder - \
-`docker-compose up -d --force-recreate --build`
+## Deploy locally
+Set token variable -
+`$env:TG_TOKEN='...:...'`
 
+Run -
+```console
+python3 main.py
+```
 
+## Deploy from docker locally
+Contents of '/var/lib/kepirchik/.env' -
+```console
+TG_TOKEN=...:...
+MODE=grisha
+DB_PATH=/var/lib/kepirchik/kefirchik.db
+```
+
+Run -
+```console
+docker-compose up --env-file '/var/lib/kepirchik/.env' -d --force-recreate --build
+```
+
+## Deploy from docker on server
+Contents of '/var/lib/kepirchik/.env' -
+```console
+TG_TOKEN=...:...
+MODE=release
+```
+
+Run -
+```console
+docker-compose up --env-file '/var/lib/kepirchik/.env' -d --force-recreate --build
+```
