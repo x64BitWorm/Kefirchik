280291715325c0f4b1e2cfbf67b131c6a8137f41
Artyom Barsov <42358981+artem-barsov@users.noreply.github.com>
2025-05-21 22:05:25 +0300

    [002] reports with uncompleted spendings (#28)
    * reports with uncompleted spendings
    
    * refactoring
    
    * tests

--- Backend/models/dto/report_dto.py
+++ Backend/models/dto/report_dto.py
@@ -1,8 +1,8 @@
 class ReportOverviewDto:
-    def __init__(self):
-        self.papiks: dict[str, float] = {}
-        self.debtors: dict[str, float] = {}
-        self.balances: dict[str, float] = {}
+    def __init__(self, papiks: dict[str, float], debtors: dict[str, float], balances: dict[str, float]):
+        self.papiks = papiks
+        self.debtors = debtors
+        self.balances = balances
 
 class ReportTransactionDto:
     def __init__(self, from_nick: str = '', to_nick: str = '', amount: float = 0.0):
@@ -11,6 +11,6 @@ class ReportTransactionDto:
         self.amount = amount
 
 class ReportInfoDto:
-    def __init__(self, transactions: int, text: str):
-        self.transactions = transactions
+    def __init__(self, transactions_count: int, text: str):
+        self.transactions_count = transactions_count
         self.text = text
--- Backend/tests/main_flow_test.py
+++ Backend/tests/main_flow_test.py
@@ -39,6 +39,74 @@ class TestSpendings(unittest.IsolatedAsyncioTestCase):
         
         await emu.sendMessage('alice', '/report')
         self.assertEqual('⚠️ Нет записанных трат', emu.getRepliedText())
+    
+    async def test_uncompleted_report(self):
+        emu = ChatEmu()
+
+        await emu.sendMessage('bob', '/add 200\n@alice 200\ntea')
+        self.assertEqual('Запомнил🍶', emu.getRepliedText())
+        
+        await emu.sendMessage('alice', '/report')
+        self.assertEqual('alice ➡️ bob 200.0🎪\n', emu.getRepliedText())
+
+        await emu.sendMessage('alice', '/add 100\n@я 30\n@bob @eve\nkefir')
+        self.assertEqual('Запомнил🍶 ждем  @bob @eve', emu.getRepliedText())
+
+        await emu.sendMessage('bob', '/report')
+        self.assertEqual('❗️ Есть незакрытая трата у @bob @eve\n\nalice ➡️ bob 200.0🎪\n', emu.getRepliedText())
+
+        await emu.sendMessage('eve', '50', reply_id=6)
+        self.assertEqual(constants.ReactionEmoji.THUMBS_UP, emu.getReaction())
+
+        await emu.sendMessage('alice', '/report')
+        self.assertEqual('❗️ Есть незакрытая трата у @bob\n\nalice ➡️ bob 200.0🎪\n', emu.getRepliedText())
+
+        await emu.sendMessage('bob', '20', reply_id=6)
+        self.assertEqual(constants.ReactionEmoji.FIRE, emu.getReaction())
+
+        await emu.sendMessage('eve', '/report')
+        self.assertEqual('alice ➡️ bob 130.0🎪\neve ➡️ bob 50.0🎪\n', emu.getRepliedText())
+
+
+    async def test_random_uncompleted_report(self):
+        emu = ChatEmu()
+
+        await emu.sendMessage('bob', '/add 200\n@alice @eve\ntea')
+        self.assertEqual('Запомнил🍶 ждем  @alice @eve', emu.getRepliedText())
+        
+        await emu.sendMessage('alice', '/add 100\n@eve @bob\nkefir')
+        self.assertEqual('Запомнил🍶 ждем  @eve @bob', emu.getRepliedText())
+        
+        await emu.sendMessage('eve', '/add 50\n@bob x\ncofe')
+        self.assertEqual('Запомнил🍶', emu.getRepliedText())
+
+        await emu.sendMessage('bob', '/report')
+        replied_text = emu.getRepliedText()
+        self.assertTrue(any(
+            expected == replied_text
+            for expected in [
+                '❗️ Есть незакрытая трата у @alice @eve\n\nbob ➡️ eve 50.0🎪\n',
+                '❗️ Есть незакрытая трата у @eve @bob\n\nbob ➡️ eve 50.0🎪\n'
+            ]
+        ))
+
+        await emu.sendMessage('alice', '50', reply_id=2)
+        self.assertEqual(constants.ReactionEmoji.THUMBS_UP, emu.getReaction())
+
+        await emu.sendMessage('eve', '150', reply_id=2)
+        self.assertEqual(constants.ReactionEmoji.FIRE, emu.getReaction())
+
+        await emu.sendMessage('alice', '/report')
+        self.assertEqual('❗️ Есть незакрытая трата у @eve @bob\n\neve ➡️ bob 100.0🎪\nalice ➡️ bob 50.0🎪\n', emu.getRepliedText())
+
+        await emu.sendMessage('bob', '80', reply_id=4)
+        self.assertEqual(constants.ReactionEmoji.THUMBS_UP, emu.getReaction())
+
+        await emu.sendMessage('eve', '20', reply_id=4)
+        self.assertEqual(constants.ReactionEmoji.FIRE, emu.getReaction())
+
+        await emu.sendMessage('eve', '/report')
+        self.assertEqual('eve ➡️ bob 70.0🎪\neve ➡️ alice 50.0🎪\n', emu.getRepliedText())
 
 if __name__ == "__main__":
     unittest.main()
